40c09fc9377f9fa48844fc587643859a619c0f77
Admin <klauss.elephant@gmail.com>
2022-04-25 11:06:30 +0700

    Article.vue has been changed

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -7,7 +7,7 @@
           <router-link
               :to="{name: 'userProfile', params: {slug: article.author.username}}"
           >
-            <img :src="article.author.image" alt="author"/>
+            <img :src="article.author.image" alt="author" />
           </router-link>
           <div class="info">
             <router-link
@@ -20,7 +20,7 @@
             </router-link>
             <span class="date">{{ article.createdAt }}</span>
           </div>
-          <span>
+          <span v-if="isAuthor">
             <router-link
                 class="btn btn-outline-secondary btn-sm"
                 :to="{name: 'editArticle', params: {slug: article.slug}}"
@@ -28,7 +28,10 @@
               <i class="ion-edit" />
               Edit Article
             </router-link>
-            <button class="btn btn-outline-danger btn-sm">
+            <button
+                class="btn btn-outline-danger btn-sm"
+                @click="deleteArticle"
+            >
               <i class="ion-trash-a" />
               Delete Article
             </button>
@@ -52,13 +55,15 @@
 </template>
 
 <script>
-import {actionTypes} from '@/store/modules/article'
-import {mapState} from 'vuex';
-import McvLoading from '@/components/loading'
-import McvErrorMessage from '@/components/loading'
+import {mapState, mapGetters} from 'vuex'
+
+import {actionTypes as articleActionTypes} from '@/store/modules/article'
+import {getterTypes as authGetterTypes} from '@/store/modules/auth'
+import McvLoading from '@/components/Loading'
+import McvErrorMessage from '@/components/ErrorMessage'
 
 export default {
-  name: "McvArticle",
+  name: 'McvArticle',
   components: {
     McvLoading,
     McvErrorMessage
@@ -68,15 +73,33 @@ export default {
       isLoading: state => state.article.isLoading,
       article: state => state.article.data,
       error: state => state.article.error
-    })
+    }),
+    ...mapGetters({
+      currentUser: authGetterTypes.currentUser
+    }),
+    isAuthor() {
+      if (!this.currentUser || !this.article) {
+        return false
+      }
+
+      return this.currentUser.username === this.article.author.username
+    }
   },
   mounted() {
-    console.log(this.$route)
-    this.$store.dispatch(actionTypes.getArticle, {slug: this.$route.params.slug})
+    this.$store.dispatch(articleActionTypes.getArticle, {
+      slug: this.$route.params.slug
+    })
+  },
+  methods: {
+    deleteArticle() {
+      this.$store
+          .dispatch(articleActionTypes.deleteArticle, {
+            slug: this.$route.params.slug
+          })
+          .then(() => {
+            this.$router.push({name: 'globalFeed'})
+          })
+    }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
